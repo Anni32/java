8d368ef0d49af447139b4ce350cdf49435de83fd
Anni32 <asheis@gmail.com>
2024-12-16 11:26:08 -0800

    Create 1768-merge_strings_alternatively.java

---
+++ leetcode/programming_skills/1768-merge_strings_alternatively.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+    int theLength = 0;
+    String endResult = "";
+// the following section compares both words, finds the longest one, and stores the length in theLength
+    if (word1.length() >= word2.length()){
+        theLength += word1.length();
+    } else {
+        theLength += word2.length();
+    }
+// the following section loops through both word 1 and word 2 characters one at a time until they are pieced together
+    for (int i = 0; i < theLength; i++){
+        if (i < word1.length()){
+            endResult += word1.charAt(i);
+        }
+        if (i < word2.length()){
+            endResult += word2.charAt(i);
+        }
+    }return endResult;
+}
+}
