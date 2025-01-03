6d830fc834e4240e61556a780423c236b6b2ef3f
Anni32 <asheis@gmail.com>
2025-01-03 12:39:25 -0800

    Update 28-find_the_index_of_the_first_occurrence_in_a_string.java

--- leetcode/programming_skills/28-find_the_index_of_the_first_occurrence_in_a_string.java
+++ leetcode/programming_skills/28-find_the_index_of_the_first_occurrence_in_a_string.java
@@ -5,17 +5,17 @@ class Solution {
         int hayslength = haystack.length();
         int needlength = needle.length();
 
-        for (int i = 0; i <= hayslength - needlength; i++) {
-            int p;
+        for (int i = 0; i <= hayslength - needlength; i++) { //iterates through the max possible starting point including window (length of haystack - length of needle)
+            int p; //initalizes p outside of loop so it can be using later in the if statement
             for (p = 0; p < needlength; p++) {
-                if (hays[i + p] != need[p]) {
+                if (hays[i + p] != need[p]) { //if hays at index i+p(starting point) doesn't equal need at p break loop.
                     break;
                 }
             }
-            if (p == needlength) {
+            if (p == needlength) { //if index p value is equal to the length of the "needle" word, return the initial position of i
                 return i;
             }
         }
-        return -1;
+        return -1; //else return -1
     }
 }
